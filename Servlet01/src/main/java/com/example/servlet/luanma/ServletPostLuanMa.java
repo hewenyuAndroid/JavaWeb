05963a92298f7f64a206205bb65fccb02246d096
hewenyu <he875027547@126.com>
2025-09-09 23:40:11 +0800

    post请求，请求体乱码问题处理

--- Servlet01/src/main/java/com/example/servlet/luanma/ServletPostLuanMa.java
+++ Servlet01/src/main/java/com/example/servlet/luanma/ServletPostLuanMa.java
@@ -13,6 +13,12 @@ public class ServletPostLuanMa extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // tomcat 10 默认以 UTF-8 为请求体的解码字符集
+        // 客户端提交数据时，要是以其它字符集对请求体中的数据进行编码就会出现乱码
+
+        // 设置请求体的解码字符集
+//        req.setCharacterEncoding("GBK");
+
         String username = req.getParameter("username");
         String parameter = req.getParameter("password");
         System.out.println("username: " + username + ", parameter: " + parameter);
