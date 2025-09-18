8a48026650388f3ad454dbb7e84b6122fc6959cf
hewenyu <he875027547@126.com>
2025-09-18 14:54:40 +0800

    新增手动连接jdbc查询的case

--- Schedule-System/src/test/java/com/example/schedule/test/DaoTest.java
+++ Schedule-System/src/test/java/com/example/schedule/test/DaoTest.java
@@ -4,9 +4,42 @@ import com.example.schedule.dao.BaseDao;
 import com.example.schedule.pojo.SysUser;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
 import java.util.List;
+import java.util.Properties;
 
 public class DaoTest {
+
+    @Test
+    public void testQuery() {
+        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(is);
+            String driverClassName = properties.getProperty("driverClassName");
+            String url = properties.getProperty("url");
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+            Class.forName(driverClassName);
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from sys_user");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            while (resultSet.next()) {
+                int dbUid = resultSet.getInt(1);
+                String dbUsername = resultSet.getString(2);
+                String dbUserPwd = resultSet.getString(3);
+                SysUser sysUser = new SysUser(dbUid, dbUsername, dbUserPwd);
+                System.out.println("sysUser = " + sysUser);
+            }
+            System.out.println("columnCount:" + columnCount);
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void testQueryCount() {
