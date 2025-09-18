71bfad65bd5a100db60e2e0a77ff9866c76f1438
hewenyu <he875027547@126.com>
2025-09-18 14:24:15 +0800

    新增对象查询方法测试

--- Schedule-System/src/test/java/com/example/schedule/test/DaoTest.java
+++ Schedule-System/src/test/java/com/example/schedule/test/DaoTest.java
@@ -1,7 +1,10 @@
 package com.example.schedule.test;
 
 import com.example.schedule.dao.BaseDao;
+import com.example.schedule.pojo.SysUser;
 import org.junit.Test;
+
+import java.util.List;
 
 public class DaoTest {
 
@@ -10,6 +13,14 @@ public class DaoTest {
         BaseDao baseDao = new BaseDao();
         Long count = baseDao.baseQueryObject(Long.class, "select count(*) from sys_user");
         System.out.println("count:" + count);
+    }
+
+    @Test
+    public void testQueryObj() {
+        BaseDao baseDao = new BaseDao();
+        // 使用别名的方式解决数据库字段 user_pwd 和 java 字段 userPwd 映射失败的问题
+        List<Object> list = baseDao.baseQuery(SysUser.class, "select uid, username, user_pwd as userPwd from sys_user where uid = 1");
+        System.out.println("result:" + list);
     }
 
 }
